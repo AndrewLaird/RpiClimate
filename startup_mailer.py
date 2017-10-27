8cc0ff4a1ef56d1bfcef1a4700778e26fff9f798
Andrew Laird <alaird@uci.edu>
2017-10-27 11:49:49 -0700

    bug fixes

--- startup_mailer.py
+++ startup_mailer.py
@@ -27,8 +27,8 @@ def connect_type(word_list):
 # Change to your own account information
 # Account Information
 to = 'lairdandrew11@gmail.com' # Email to send to.
-gmail_user = 'lairdandrew11@gmail.com' # Email to send from. (MUST BE GMAIL)
-gmail_password = base64.b64decode('cGluaypveDE=') # Gmail password.
+gmail_user = 'INRFPI@gmail.com' # Email to send from. (MUST BE GMAIL)
+gmail_password = base64.b64decode('UGlJTlJGMjAxNw==') # Gmail password.
 smtpserver = smtplib.SMTP('smtp.gmail.com', 587) # Server to use.
 
 
