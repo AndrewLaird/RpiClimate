bff414e1336d4aaf5060eda2c3ecab89f0251b71
Andrew Laird <alaird@uci.edu>
2017-11-27 13:31:24 -0800

    ci test

---
+++ upgrade.sh
@@ -0,0 +1,4 @@
+#this file takes the directory in side of ~/upgrade and replaces everything in RPiClimate with its new copy
+
+cp -R ~/upgrade/RPiClimate ~/production/RPiClimate
+echo "upgrade complete"
--- upgrade_pull.py
+++ upgrade_pull.py
@@ -1,14 +1,19 @@
-import urllib.request
+from urllib.request import urlretrieve
 import zipfile
 
 #determine wheter an upgrade is necessary
+build_url = "None"
+build_url = "http://poems.calit2.uci.edu/poems/RPiClimate_update/2017/11/27/RpiClimate.zip"
 
 #if it is use the url provided to download the zipped file
-if(False):
-    zip_ref = zipfile.ZipFile("necessary path", 'r')
-    zip_ref.extractall("~/home/upgrade")
+if(build_url != "None"):
+    #downloading the zipfile
+    #build = requests.urlopen(build_url)
+    #build_zip = open("/home/andrew/upgrade/build.zip","w")
+    urlretrieve(build_url, "/home/andrew/upgrade/build.zip")
+
+    #unzip the file in a folder called upgrade
+    zip_ref = zipfile.ZipFile("/home/andrew/upgrade/build.zip", 'r')
+    zip_ref.extractall("/home/andrew/upgrade")
     zip_ref.close()
-
-#unzip the file in a folder called upgrade
-
-#then in a shell script outside of RPiClimate, replace the folders and directories with the new upgraded ones
+    #then in a shell script outside of RPiClimate, replace the folders and directories with the new upgraded ones
