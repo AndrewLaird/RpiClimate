8c1874b264bf38521f99352c5f3e5a6abdbfa689
Andrew Laird <alaird@uci.edu>
2017-12-06 10:38:50 -0800

    working on upgrading shell/python scripts

--- upgrade.sh
+++ upgrade.sh
@@ -1,4 +1,12 @@
 #this file takes the directory in side of ~/upgrade and replaces everything in RPiClimate with its new copy
+MAC="$(ifconfig -a | grep -Po 'HWaddr \K.*$')"
+echo $MAC
+RESULT="$(python3 ~/RpiClimate/upgrade_pull.py $MAC)"
+echo $RESULT
 
-cp -R ~/upgrade/RPiClimate ~/production/RPiClimate
-echo "upgrade complete"
+if [$RESULT=="Sucess"]; then
+    cp -R ~/upgrade/RPiClimate ~/production/RPiClimate
+    echo "upgrade complete"
+else
+    echo "up to date"
+fi
--- upgrade_pull.py
+++ upgrade_pull.py
@@ -1,19 +1,26 @@
 from urllib.request import urlretrieve
 import zipfile
+import sys
 
-#determine wheter an upgrade is necessary
-build_url = "None"
-build_url = "http://poems.calit2.uci.edu/poems/RPiClimate_update/2017/11/27/RpiClimate.zip"
 
-#if it is use the url provided to download the zipped file
-if(build_url != "None"):
-    #downloading the zipfile
-    #build = requests.urlopen(build_url)
-    #build_zip = open("/home/andrew/upgrade/build.zip","w")
-    urlretrieve(build_url, "/home/andrew/upgrade/build.zip")
+if __name__ ==  "__main__":
+    # determine wheter an upgrade is necessary
+    mac = sys.argv[1]
+    print(mac)
+    build_url = "None"
+    build_url = "http://poems.calit2.uci.edu/poems/RPiClimate_update/2017/11/27/RpiClimate.zip"
 
-    #unzip the file in a folder called upgrade
-    zip_ref = zipfile.ZipFile("/home/andrew/upgrade/build.zip", 'r')
-    zip_ref.extractall("/home/andrew/upgrade")
-    zip_ref.close()
-    #then in a shell script outside of RPiClimate, replace the folders and directories with the new upgraded ones
+    # if it is use the url provided to download the zipped file
+    if (build_url != "None"):
+        # downloading the zipfile
+        # build = requests.urlopen(build_url)
+        # build_zip = open("/home/andrew/upgrade/build.zip","w")
+        urlretrieve(build_url, "/home/andrew/upgrade/build.zip")
+
+        # unzip the file in a folder called upgrade
+        zip_ref = zipfile.ZipFile("/home/andrew/upgrade/build.zip", 'r')
+        zip_ref.extractall("/home/andrew/upgrade")
+        zip_ref.close()
+        # then in a shell script outside of RPiClimate, replace the folders and directories with the new upgraded ones
+        print("Sucess") #using std out to interface with shell
+    print("None")#using std out to interface with shell
