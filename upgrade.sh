69e9bc0e4c5c6a0459d44dab48bd5bb0169a719c
Andrew Laird <alaird@uci.edu>
2017-12-06 14:56:13 -0800

    spelling

--- upgrade.sh
+++ upgrade.sh
@@ -6,7 +6,7 @@ echo "result"
 echo $RESULT
 
 if [ "$RESULT" = "Sucess" ]; then
-    cp -R /home/pi/upgrade/RPiClimate /home/pi/production/RPiClimate
+    cp -R /home/pi/upgrade/RpiClimate /home/pi/production/RpiClimate
     echo "upgrade complete"
 else
     echo "up to date"
