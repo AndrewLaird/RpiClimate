02b9e0014991ad4fa3d19695c15e4b2b3f5c44ea
Andrew Laird <alaird@uci.edu>
2018-02-16 12:26:35 -0800

    pushing network changes

--- network_setup.sh
+++ network_setup.sh
@@ -1,8 +1,9 @@
+
 #first change wpa_supplicant
-sudo cp /home/pi/RpiClimate/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
+sudo bash -c "cat /home/pi/RpiClimate/wpa_supplicant.conf >> /etc/wpa_supplicant/wpa_supplicant.conf"
 
 #then change the interfaces to use that file
-sudo cp /home/pi/RpiClimate/interfaces /etc/network/interfaces
+sudo bash -c "cat /home/pi/RpiClimate/interfaces >> /etc/network/interfaces"
 
 #then restart the network to get it running
 sudo ifconfig wlan1 down
